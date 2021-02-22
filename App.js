2849e353d54e6268cd7ddd247918e06ed2749c6f
manci <gmancillapaz@gmail.com>
2021-02-22 16:14:38 -0500

    citas metodo map

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState} from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 
 
 const App = () => {
+  const [citas, setCitas] = useState([
+    { id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'No Come'},
+    { id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'No Duerme'},
+    { id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'No Conta'},
+  ]);
+
   return (
-    <Text>Proyecto Citas</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.title}>Administrador Citas</Text>
+      {citas.map(cita => (
+        <View>
+          <Text>{cita.paciente}</Text>
+        </View>
+      ))}
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#aa076b',
+    flex: 1
+  },
+  title: {
+    color: '#fff',
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
 export default App;
