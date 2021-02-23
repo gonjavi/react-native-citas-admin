763afdd2a05638c97a451ca89d8089f2c8d722d3
manci <gmancillapaz@gmail.com>
2021-02-22 20:33:12 -0500

    crear calendario con instalacion de paquete

--- componentes/Formulario.js
+++ componentes/Formulario.js
@@ -1,7 +1,23 @@
 import React, { useState} from 'react';
-import { Text, StyleSheet, View, TextInput } from 'react-native';
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Formulario = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+
  return (
    <>
     <View style={styles.formulario}>
@@ -30,6 +46,15 @@ const Formulario = () => {
         />
       </View>
 
+      <View>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+      </View>
       <View>
         <Text style={styles.label}>Sintomas:</Text>
         <TextInput
